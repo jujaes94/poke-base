1a9df606796a2534b73759d5bc4eabbad7cd53de
jujaes94 <50428313+jujaes94@users.noreply.github.com>
2021-08-28 14:10:17 -0500

    set docker config

--- app/db/config.py
+++ app/db/config.py
@@ -1,11 +1,15 @@
 from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.db.tables import Base
+from pydantic import Field
+import os
 
 print('Stablish connection with DB')
 print('Getting engie...')
+URL = os.getenv('DATABASE_URL')
 # coneción al engie postgres
-engie = create_engine('postgresql://postgres:admin@localhost/movies')
+# engie = create_engine('postgresql://postgres:admin@localhost/movies')
+engie = create_engine(URL)
 # declaración de base
 
 print('Getting session...')
--- app/db/tables.py
+++ app/db/tables.py
@@ -61,16 +61,3 @@ class Pokemons_table(Base):
 
     def __str__(self):
         return self.name
-
-
-class Errors(Base):
-    # se definino nombre de la tabla
-    __tablename__ = 'errors'
-
-    # se define campos de la tabla como atributos
-    id = Column(Integer(),primary_key=True)
-    code = Column(Integer(), nullable=False)
-    detail = Column(String(50))
-
-    def __str__(self):
-        return self.code
